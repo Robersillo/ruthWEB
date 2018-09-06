88ac4517057f59f99814b1498c4ac51666276999
Robersillo <31243904+Robersillo@users.noreply.github.com>
2018-09-06 13:24:48 +0200

    Creacio nde Header y eliminar HTML INDEX

--- html/index/index.php
+++ html/index/index.php
@@ -1,33 +1,5 @@
 <!doctype html>
-<!--[if IE 7 ]>    <html lang="en-gb" class="isie ie7 oldie no-js"> <![endif]-->
-<!--[if IE 8 ]>    <html lang="en-gb" class="isie ie8 oldie no-js"> <![endif]-->
-<!--[if IE 9 ]>    <html lang="en-gb" class="isie ie9 no-js"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!-->
-<html lang="en-gb" class="no-js">
-<!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<!--[if lt IE 9]>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <![endif]-->
-<title>Free-Portfolio-HTML5-Responsive-Website-Sam</title>
-<meta name="description" content="">
-<meta name="author" content="WebThemez">
-<!--[if lt IE 9]>
-        <script src="views/http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-<!--[if lte IE 8]>
-		<script type="text/javascript" src="views/http://explorercanvas.googlecode.com/svn/trunk/excanvas.js"></script>
-	<![endif]-->
-<link rel="stylesheet" href="views/css/bootstrap.min.css" />
-<link rel="stylesheet" type="views/text/css" href="css/isotope.css" media="screen" />
-<link rel="stylesheet" href="views/js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
-<link rel="stylesheet" type="views/text/css" href="css/da-slider.css" />
-<link rel="stylesheet" href="views/css/styles.css" />
-<!-- Font Awesome -->
-<link href="views/css/font-awesome.min.css" rel="stylesheet">
-</head>
+<?php include ('html/overall/header.php');?>
 
 <body>
 <header class="header">
--- html/overall/header.php
+++ html/overall/header.php
@@ -1,4 +1,7 @@
+<!doctype html>
+
 <html lang="en-gb" class="no-js">
+
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
