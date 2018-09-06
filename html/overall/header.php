9681f777ec2e2acaec0f7fa84154746eb65c36cc
Robersillo <31243904+Robersillo@users.noreply.github.com>
2018-09-06 13:09:53 +0200

    COMIENZO

---
+++ html/overall/header.php
@@ -0,0 +1,17 @@
+<html lang="en-gb" class="no-js">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Free-Portfolio-HTML5-Responsive-Website-Sam</title>
+<meta name="description" content="">
+<meta name="author" content="WebThemez">
+
+<link rel="stylesheet" href="views/css/bootstrap.min.css" />
+<link rel="stylesheet" type="views/text/css" href="css/isotope.css" media="screen" />
+<link rel="stylesheet" href="views/js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+<link rel="stylesheet" type="views/text/css" href="css/da-slider.css" />
+<link rel="stylesheet" href="views/css/styles.css" />
+<!-- Font Awesome -->
+<link href="views/css/font-awesome.min.css" rel="stylesheet">
+</head>
